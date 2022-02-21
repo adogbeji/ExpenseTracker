1ac98575f489809e95ee275f98851d0a62cdd364
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-02-21 16:46:59 +0000

    Ouput children prop & pass classes from Expense Item --> Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,8 @@
 import './Card.css';
 
-function Card() {
-    return <div className="card"></div>;
+function Card(props) {
+    const classes = 'card ' + props.className;  // Any values for className added to the string
+    return <div className={classes}>{props.children}</div>;
 }
 
 export default Card;
