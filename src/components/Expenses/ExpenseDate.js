fcc329ebfa4781373caee975644dc9b6e1de7ef1
Benjamin Akponasa <baka_1993@yahoo.co.uk>
2022-04-22 12:23:20 +0100

    Organise components into separate folders & update import paths

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -3,8 +3,8 @@ import './ExpenseDate.css';
 
 function ExpenseDate(props) {
   // Helper constants
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
   const year = props.date.getFullYear();
 
   return (
